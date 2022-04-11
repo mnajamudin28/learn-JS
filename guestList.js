cdfd0adc7f3ad600c63530ee85b717f3d5e5490c
Moe <43162722+mnajamudin28@users.noreply.github.com>
2022-04-12 02:32:26 +0700

    Create guestList.js

---
+++ guestList.js
@@ -0,0 +1,8 @@
+var guestList =["Angela","Jakc", "Pan", "James","Lara","Jason"];
+var guestName = prompt("What is your name?");
+
+if(guestList.includes(guestName)){
+   alert("Welcome!");
+}else{
+   alert("Sorry, maybe nexxt time");
+}
