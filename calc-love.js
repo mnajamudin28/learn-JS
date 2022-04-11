5d26e7854d83c038ebf8ee04d5bae2913150fe92
Moe <43162722+mnajamudin28@users.noreply.github.com>
2022-04-12 01:53:02 +0700

    Create calc-love.js

---
+++ calc-love.js
@@ -0,0 +1,18 @@
+prompt("What is your name?");
+prompt ("What is thir name?");
+
+var loveScore = Math.random() * 100;
+loveScore = Math.floor(loveScore) + 1;
+
+if(loveScore > 70){
+        alert("your love score is " + loveScore+ "%"+" You love each other");
+    }
+
+if (loveScore > 30 && loveScore <= 70){
+        alert("your love score is " + loveScore+ "%");
+    }
+
+if (loveScore <= 30){
+       alert("your love score is " + loveScore+ "%"+"you go together like oil and water"); 
+    }
+    
