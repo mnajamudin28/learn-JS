ceff1a0636a8e261b2d9ec4c6374efd70f4e16f2
Moe <43162722+mnajamudin28@users.noreply.github.com>
2022-04-22 01:36:27 +0700

    Add files via upload

---
+++ intro-to-node/index.js
@@ -0,0 +1,9 @@
+const superheroes = require('superheroes');
+const supervillains = require('supervillains');
+
+
+var mySuperHero = superheroes.random();
+var mySuperVillains = supervillains.random();
+
+console.log("super hero " + mySuperHero);
+console.log("Super Villain " +mySuperVillains);
